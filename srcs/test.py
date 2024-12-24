a10a5f6a43a573cef56517eb837fce419d562925
Abdoul Rachid Ouedraogo <57127132+Rekin226@users.noreply.github.com>
2024-12-24 08:20:44 +0000

    create new functions

--- srcs/test.py
+++ srcs/test.py
@@ -11,3 +11,14 @@ if __name__ == "__main__":
     test()
 # Add hello print
 print("hello, this is just a test")
+
+# create sum function
+def sum(a, b):
+    return a + b
+
+# create sub function
+def sub(a, b):
+    return a - b
+
+# call sum function
+print(sum(10, 20))
